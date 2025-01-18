af48503b73b257e81741e946618ffbb32f99de9b
root <root@LAPTOP-97G9G880.>
2025-01-18 18:27:36 +0000

    Add heap file implementation with free space management

---
+++ database_engine/build/CMakeFiles/database_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/database_engine.dir/main.cpp.o"
+  "CMakeFiles/database_engine.dir/main.cpp.o.d"
+  "database_engine"
+  "database_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/database_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ database_engine/build/storage/CMakeFiles/slotted_page.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slotted_page.dir/src/slotted_page.cpp.o"
+  "CMakeFiles/slotted_page.dir/src/slotted_page.cpp.o.d"
+  "libslotted_page.a"
+  "libslotted_page.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slotted_page.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
